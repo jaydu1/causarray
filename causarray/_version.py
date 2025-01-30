15d4ed32e00f7dfa1dcb436b4e68960de7a0c90c
jaydu1 <413075930@qq.com>
2025-01-29 23:37:13 -0500

    Update version format

--- causarray/_version.py
+++ causarray/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.1.dev0+ga515be8.d20250130'
-__version_tuple__ = version_tuple = (0, 0, 1, 'dev0', 'ga515be8.d20250130')
+__version__ = version = '0.0.1.post1.dev0'
+__version_tuple__ = version_tuple = (0, 0, 1, 'dev0')
